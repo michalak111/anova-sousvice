5f6a52a9aa0a3e85bb88f37fda3529cace1fbe3b
michalak111 <michalak111@gmail.com>
2025-01-18 13:48:08 +0100

    feat: change

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -8,7 +8,7 @@ import { useThemeColor } from "@/hooks/useThemeColor";
 export default function IndexHome() {
   return (
     <View style={{ padding: 16, gap: 16 }}>
-      <Text>V1 - 18.01 13:20</Text>
+      <Text>V1 - 13:47</Text>
       <Card href={"/home/guides"} title={"Guides"} />
       <Card href={"/home/history"} title={"History"} />
     </View>
